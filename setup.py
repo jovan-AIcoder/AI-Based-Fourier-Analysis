c927b42ce03881dcb0c061a6c25013eec529f62f
jovan-AIcoder <jovanpermana10@gmail.com>
2026-05-09 06:41:33 +0800

    fixing bugs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aifourier",
-    version="0.4.0",
+    version="0.4.1",
     description="AI-based Fourier Analysis using sinusoidal neural networks",
     author="Jovan",
     packages=find_packages(),
