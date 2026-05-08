884141e08bc0d210dc530c73d3df78c51a73866c
jovan-AIcoder <jovanpermana10@gmail.com>
2026-05-08 18:54:03 +0800

    updating to version 0.2.0

--- aifourier/analyzer.py
+++ aifourier/analyzer.py
@@ -4,7 +4,7 @@ import librosa
 import numpy as np
 from tensorflow import keras
 from tensorflow import math
-def analyze(audio_path,max_modes=128,epochs=64):
+def analyze(audio_path,max_modes=128,epochs=64,use_phase_shift=True):
     # error handling
     if not isinstance(audio_path, str):
         raise ValueError('Audio path must be a string.')
@@ -12,6 +12,8 @@ def analyze(audio_path,max_modes=128,epochs=64):
         raise ValueError('Max modes must be a positive integer.')
     if not isinstance(epochs, int) or epochs <= 0:
         raise ValueError('Epochs must be a positive integer.')
+    if not isinstance(use_phase_shift,bool):
+        raise ValueError('use_phase_shift must be a boolean')
     try:
         if not os.path.isfile(audio_path):
             raise ValueError("Audio file not found.")
@@ -30,16 +32,23 @@ def analyze(audio_path,max_modes=128,epochs=64):
     print('Audio signal sampled successfully.')
     # analyze the audio signal
     model = keras.Sequential([
-        keras.layers.Dense(max_modes, activation=math.sin, input_shape=(1,)),
+        keras.layers.Dense(max_modes, activation=math.sin, input_shape=(1,),use_bias=use_phase_shift),
         keras.layers.Dense(1,use_bias=False)
     ])
     model.compile(optimizer='adam', loss='mean_squared_error')
     print('Analyzing the audio signal...')
     model.fit(t.values.reshape(-1, 1), y.values, epochs=epochs)
-    weights, biases = model.layers[0].get_weights()
-    amplitudes = model.layers[1].get_weights()[0]
-    weights_flat = weights[0]
-    df_freq = pd.DataFrame({'Frequencies': weights_flat,'Phase shift': biases,'Amplitudes': amplitudes.flatten()})
-    df_freq.index = [f'Frequency_{i}' for i in range(len(biases))]
+    if use_phase_shift:
+        weights, biases = model.layers[0].get_weights()
+        amplitudes = model.layers[1].get_weights()[0]
+        weights_flat = weights[0]
+        df_freq = pd.DataFrame({'Frequencies': weights_flat,'Phase shift': biases,'Amplitudes': amplitudes.flatten()})
+        df_freq.index = [f'Frequency_{i}' for i in range(len(biases))]
+    else:
+        weights = model.layers[0].get_weights()
+        amplitudes = model.layers[1].get_weights()[0]
+        weights_flat = weights[0]
+        df_freq = pd.DataFrame({'Frequencies': weights_flat,'Amplitudes': amplitudes.flatten()})
+        df_freq.index = [f'Frequency_{i}' for i in range(len(weights))]
     print('Audio signal analyzed successfully.')
     return df_freq
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aifourier",
-    version="0.1.0",
+    version="0.2.0",
     description="AI-based Fourier Analysis using sinusoidal neural networks",
     author="Jovan",
     packages=find_packages(),
